313c26d8efc874960414e8a5604f0ab12a97f8a2
cthacker-udel <cthacker@udel.edu>
2022-08-06 11:25:07 -0400

    Updating App

---
+++ src/modules/Dashboard/Dashboard.tsx
@@ -0,0 +1,106 @@
+import {
+    faBook,
+    faBookAtlas,
+    faClipboard,
+    faRectangleList,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
+import React from "react";
+import { OverlayTrigger } from "react-bootstrap";
+import type { OverlayInjectedProps } from "react-bootstrap/esm/Overlay";
+import { generateTooltip } from "src/helpers";
+
+import styles from "./Dashboard.module.css";
+
+/**
+ * General dashboard for navigating between user applications
+ *
+ * @returns The dashboard component
+ */
+export const Dashboard = (): JSX.Element => {
+    const router = useRouter();
+
+    return (
+        <div className="h-100 mx-auto w-50 d-flex flex-column align-items-center justify-content-center">
+            <div className="fs-3 fw-bolder text-center mb-4 text-wrap border-bottom pb-1">
+                {"Course Scheduler Dashboard"}
+            </div>
+            <div>
+                <div className="d-flex flex-row">
+                    <span
+                        className="p-3"
+                        onClick={async (): Promise<void> => {
+                            await router.push("/dashboard/course");
+                        }}
+                    >
+                        <OverlayTrigger
+                            delay={{ hide: 100, show: 500 }}
+                            overlay={(
+                                props: OverlayInjectedProps,
+                            ): JSX.Element => generateTooltip("Courses", props)}
+                            placement="left"
+                        >
+                            <FontAwesomeIcon
+                                className={`p-5 border ${styles.dashboard_icon} rounded`}
+                                icon={faBook}
+                                size="3x"
+                            />
+                        </OverlayTrigger>
+                    </span>
+                    <span className="p-3">
+                        <OverlayTrigger
+                            delay={{ hide: 100, show: 500 }}
+                            overlay={(
+                                props: OverlayInjectedProps,
+                            ): JSX.Element =>
+                                generateTooltip("Semesters", props)
+                            }
+                            placement="right"
+                        >
+                            <FontAwesomeIcon
+                                className={`p-5 border ${styles.dashboard_icon} rounded`}
+                                icon={faBookAtlas}
+                                size="3x"
+                            />
+                        </OverlayTrigger>
+                    </span>
+                </div>
+                <div className="d-flex flex-row">
+                    <span className="p-3">
+                        <OverlayTrigger
+                            delay={{ hide: 100, show: 500 }}
+                            overlay={(
+                                props: OverlayInjectedProps,
+                            ): JSX.Element => generateTooltip("Plans", props)}
+                            placement="left"
+                        >
+                            <FontAwesomeIcon
+                                className={`p-5 border ${styles.dashboard_icon} rounded`}
+                                icon={faClipboard}
+                                size="3x"
+                            />
+                        </OverlayTrigger>
+                    </span>
+                    <span className="p-3">
+                        <OverlayTrigger
+                            delay={{ hide: 100, show: 500 }}
+                            overlay={(
+                                props: OverlayInjectedProps,
+                            ): JSX.Element =>
+                                generateTooltip("Courses List", props)
+                            }
+                            placement="right"
+                        >
+                            <FontAwesomeIcon
+                                className={`p-5 border ${styles.dashboard_icon} rounded`}
+                                icon={faRectangleList}
+                                size="3x"
+                            />
+                        </OverlayTrigger>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
